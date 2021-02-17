8473904f65b4fedf32b7eab5ae8ebff3b7cc0837
adrian1230 <adrian1145d98@gmail.com>
2021-02-18 05:38:02 +0800

    adrian

--- src/yfinance.compare.py
+++ src/yfinance.compare.py
@@ -84,6 +84,20 @@ st.write("""
 st.header("Your selected stocks")
 lit
 
+open_tick = []
+
+for j in range(len(lit)):
+    data = yf.Ticker(lit[j])
+    df = data.history(period=hist[0],start=hist[1],end=hist[2])
+    open_tick.append(df.Open)
+
+length = 0
+
+for f in range(len(open_tick)):
+    length += len(open_tick[f])
+
+open_tick = np.array(open_tick).reshape((int(length/len(lit)),len(lit)))
+
 close_tick = []
 
 for j in range(len(lit)):
@@ -91,15 +105,57 @@ for j in range(len(lit)):
     df = data.history(period=hist[0],start=hist[1],end=hist[2])
     close_tick.append(df.Close)
 
-length = 0
-
-for f in range(len(close_tick)):
-    length += len(close_tick[f])
-
 close_tick = np.array(close_tick).reshape((int(length/len(lit)),len(lit)))
+
+high_tick = []
+
+for j in range(len(lit)):
+    data = yf.Ticker(lit[j])
+    df = data.history(period=hist[0],start=hist[1],end=hist[2])
+    high_tick.append(df.High)
+
+high_tick = np.array(high_tick).reshape((int(length/len(lit)),len(lit)))
+
+low_tick = []
+
+for j in range(len(lit)):
+    data = yf.Ticker(lit[j])
+    df = data.history(period=hist[0],start=hist[1],end=hist[2])
+    low_tick.append(df.Low)
+
+low_tick = np.array(low_tick).reshape((int(length/len(lit)),len(lit)))
+
+volume_tick = []
+
+for j in range(len(lit)):
+    data = yf.Ticker(lit[j])
+    df = data.history(period=hist[0],start=hist[1],end=hist[2])
+    volume_tick.append(df.Volume)
+
+volume_tick = np.array(volume_tick).reshape((int(length/len(lit)),len(lit)))
 
 Closingchart = pd.DataFrame(
     close_tick,
+    columns=[lit[d] for d in range(len(lit))]
+)
+
+Openchart = pd.DataFrame(
+    open_tick,
+    columns=[lit[d] for d in range(len(lit))]
+)
+
+Highchart = pd.DataFrame(
+    high_tick,
+    columns=[lit[d] for d in range(len(lit))]
+)
+
+Lowchart = pd.DataFrame(
+    low_tick,
+    columns=[lit[d] for d in range(len(lit))]
+)
+
+Volumechart = pd.DataFrame(
+    volume_tick,
     columns=[lit[d] for d in range(len(lit))]
 )
 
