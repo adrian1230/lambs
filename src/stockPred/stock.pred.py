86eb743abd3b9cace5b2ab69ef2fc83ac606e778
adrian1230 <adrian1145d98@gmail.com>
2021-02-19 02:59:43 +0800

    adrian

--- src/stockPred/stock.pred.py
+++ src/stockPred/stock.pred.py
@@ -4,7 +4,7 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 import keras as ks
-import scklearn as sck
+import sklearn as sk
 import datetime as dt
 
 tod = dt.date.today()
@@ -27,14 +27,16 @@ if pm1 != 1:
 today = tod.strftime("%Y-%m-%d")
 
 st.write("""
-# Compare Stocks from Yahoo Finance
+# Visualize and Predict One Stock from Yahoo Finance
 """)
 
-st.header("Enter stock codes here!")
+st.header("Enter the stock code here!")
 
-st.subheader("Example Input: AAPL, FTSE 100; 1d, 2010-03-14, 2015-03-14")
+st.subheader("Example Input: AAPL; 1d, 2010-03-14, 2015-03-14")
 
-stock_codes = "AAPL, Gold, TSLA, MSFT, GE; 1d, {}, {}".format(last,today)
+st.text("Train from 2010-03-14 to 2015-03-14")
+
+stock_codes = "GE; 1d, {}, {}".format(last,today)
 
 lit = st.text_area("Stock & History Input", stock_codes)
 
@@ -48,17 +50,34 @@ for k in range(len(hist)):
 for i in range(len(lit)):
     lit[i] = lit[i].strip()
 
-print("These are your selected stocks:")
-
-for p, q in enumerate(lit):
-    print(p, '=>', q)
+if len(lit) > 1:
+    raise ValueError("More than 1")
 
 st.write("""
 ***
 """)
 
-st.header("Your selected stocks")
-lit
+st.header("Your selected stock: {}".format(lit[0]))
 
+st.subheader("Training or Application")
+
+choice = st.text_input("Training or Application; 0 vs 1: ", 1)
+
+choice = int(choice)
+
+print(choice)
+
+if choice < 0 or choice > 1:
+    raise ValueError("other than 1 or 0")
+
+def option_(choice):
+    if choice == 0:
+        return "Training"
+    elif choice == 1:
+        return "Application"
+    else:
+        raise ValueError("Not an option")
+
+st.subheader("You chose {}".format(option_(choice)))
 
 
